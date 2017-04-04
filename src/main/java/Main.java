399c84268fd9f8cf5914ec3157635ef888f4b353
Lukasz Kolacz <lukaszkolacz15@gmail.com>
2017-04-04 19:35:01 +0200

    lombok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,40 @@
+import lombok.Cleanup;
+
+import java.io.*;
+
 /**
  * Created by Lukasz Kolacz on 03.04.2017.
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("sadasd");
-    }
 
+        Player player = new Player();
+        player.setAge(5);
+        player.setName("Lukas");
+//          !!!!!!! toString method !!!!!!
+        System.out.println(player.toString());
+
+
+//        Do @Builder w klasie Player
+
+//        Player player1 = Player.builder().age(1).name("Lukas")
+//                .age1(10)
+//                .name1("Matis")
+//                .age2(2)
+//                .name2("Jakies")
+//                .age3(5).name3("Cos").build();
+
+
+        try {
+            @Cleanup
+            InputStream in = new FileInputStream(new File("tutaj_sciezka_do_pliku"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
---
+++ src/main/java/Player.java
@@ -0,0 +1,21 @@
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Created by Lukasz Kolacz on 04.04.2017.
+ */
+
+@Data
+//@Builder
+public class Player {
+
+    private int age;
+    private String name;
+//    private int age1;
+//    private String name1;
+//    private int age2;
+//    private String name2;
+//    private int age3;
+//    private String name3;
+
+}
